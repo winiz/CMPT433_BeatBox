d3f4667de6b53e91ab669b068c6fa71e1854c4e8
winiz <winizhang1995@gmail.com>
2017-09-27 03:25:02 -0700

    almost done

--- work/as1/hello.c
+++ work/as1/hello.c
@@ -1,5 +1,6 @@
-/*
-Requirements:
+/*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$Requirements:$$$$$$$$$$$$$$$$$$$$$$$$
+
 Display hello-world welcome message
 Continuously loop through the following:
 	1. Count # consecutive iterations joystick has been pressed up
@@ -11,7 +12,8 @@ Display ending message
 
 Author: 
 Xinran Zhang 10:16AM 2017.9.26 
-*/
+
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,17 +23,22 @@ Xinran Zhang 10:16AM 2017.9.26
 #define GPIO_EXPORT_PATH "/sys/class/gpio/export"
 #define GPIO_NUMBER 26
 #define JOYSTICK_GPIO_VALUE_PATH "/sys/class/gpio/gpio26/value"
+#define LED0_TRIGGER_PATH "/sys/class/leds/beaglebone:green:usr0/trigger"
+#define LED0_DELAYON_PATH "/sys/class/leds/beaglebone:green:usr0/delay_on"
+#define LED0_DELAYOFF_PATH "/sys/class/leds/beaglebone:green:usr0/delay_off"
 
 int readFromFile(char *fileName);
 void writeToFile(char *fileName, int gpioNum);
 _Bool checkIfPressedUp();
+int ledFlashing(int mode);
 void busyWait();
+
 
 
 int main(int argc, char* argv[])
 {	
 	int counter = 0;
-	int Joystick;
+	int Joystick,mode;
 	//_Bool isPressed = checkIfPressedUp();
 	
 	printf("Hello embedded world! from Xinran Zhang!\n"); //Display hello-world welcome message
@@ -39,16 +46,18 @@ int main(int argc, char* argv[])
 	//dealing with gpio 26 JoystickUp
 		
 	writeToFile(GPIO_EXPORT_PATH,GPIO_NUMBER); //equivalent: echo 26 > export
-	while (counter < 10){
+	while (counter < 9){
 		if (checkIfPressedUp()){
+			mode = ledFlashing(3);
 			Joystick = 1;
 			counter++;
 		}
 		else if (!checkIfPressedUp()){
+			mode = ledFlashing(1);
 			Joystick = 0;
 			counter = 0;
 		}
-		printf("Flashing 1 time(s): Joystick = %d & counter = %d\n", Joystick, counter);
+		printf("Flashing %d time(s): Joystick = %d & counter = %d\n", mode, Joystick, counter);
 	busyWait();
 	}
 	printf("Bye Bye embedded world!\n");//Display ending message
@@ -67,7 +76,7 @@ int readFromFile(char *fileName){
 	//read integer
 	fscanf(pfile, "%d", &boo);  
 
-	//Close
+	//Close the file 
 	fclose(pfile);
 	return boo;
 }
@@ -98,9 +107,42 @@ _Bool checkIfPressedUp(){
 	}
 }
 
+int ledFlashing(int mode){
+	// echo timer > trigger
+	FILE *pfile = fopen(LED0_TRIGGER_PATH, "w");
+	if (pfile == NULL) {
+		printf("ERROR: Unable to open export file.\n");
+		exit(1);
+	}
+	fprintf(pfile, "%s", "timer");
+	fclose(pfile);
+	
+	//mode 0 simply means for busywait 600ms 
+	if (mode==0){
+		writeToFile(LED0_DELAYOFF_PATH, 600);
+	}
+	//mode 1 simply means flashes once eh
+	else if (mode==1){
+		writeToFile(LED0_DELAYON_PATH, 100);
+		writeToFile(LED0_DELAYOFF_PATH, 100);
+	}
+	
+	//mode 3 simply means triple kills eh
+	else if (mode==3){
+		writeToFile(LED0_DELAYON_PATH, 100);
+		writeToFile(LED0_DELAYOFF_PATH, 100);
+		writeToFile(LED0_DELAYON_PATH, 100);
+		writeToFile(LED0_DELAYOFF_PATH, 100);
+		writeToFile(LED0_DELAYON_PATH, 100);
+		writeToFile(LED0_DELAYOFF_PATH, 100);
+	}
+	return mode;
+}
+
 void busyWait(){ //hard coded to 600 ms which is 6e+8 according to duckduckgo
 	long seconds = 0;
 	long nanoseconds = 600000000;
 	struct timespec reqDelay = {seconds, nanoseconds};
 	nanosleep(&reqDelay, (struct timespec *) NULL);
+	ledFlashing(0);
 }
