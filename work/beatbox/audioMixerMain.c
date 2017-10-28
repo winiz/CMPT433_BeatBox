5498be45ba6b657e2cc63f2a92ae4e07a85aa46d
winiz <winizhang1995@gmail.com>
2017-10-28 02:50:47 -0700

    gonna go sleep

--- work/beatbox/audioMixerMain.c
+++ work/beatbox/audioMixerMain.c
@@ -4,9 +4,11 @@
 int main(void)
 {
 	printf("Beginning play-back of %s\n", SOURCE_FILE_SNARE);
-
 	// Configure Output Device
+	wavedata_t sampleFile;
 	AudioMixer_init();
+	AudioMixer_readWaveFileIntoMemory(SOURCE_FILE_SNARE, &sampleFile);
+	AudioMixer_freeWaveFileData(&sampleFile);
 	AudioMixer_cleanup();
 	printf("Done!\n");
 	return 0;
